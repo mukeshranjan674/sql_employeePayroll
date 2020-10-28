20fbbab0e347f6e0c5a3bdab0d32145bf1d85db5
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-29 00:23:37 +0530

    [Mukesh] add : table created

--- employeePayrollService.sql
+++ employeePayrollService.sql
@@ -2,3 +2,13 @@
 #DataBase Created
 CREATE DATABASE employee_payroll_service;
 
+#UC2
+#Table Created
+CREATE TABLE employee_payroll
+(
+id INT unsigned NOT NULL AUTO_INCREMENT,
+name VARCHAR(150) NOT NULL,
+salary Double NOT NULL,
+start DATE NOT NULL,
+PRIMARY KEY (id)
+);
