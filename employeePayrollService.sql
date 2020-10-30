24ba6cbef2963710107100f8a62488922d7838b5
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-31 00:56:55 +0530

    [Mukesh] add : Ability to extend employee_payroll data to store employee information like

--- employeePayrollService.sql
+++ employeePayrollService.sql
@@ -15,37 +15,45 @@ PRIMARY KEY (id)
 
 #UC3
 #Ability to insert into the table
-INSERT INTO employee_payroll_service(name, salary, start) VALUES
-('Billie', 1000000.00, '2020-02-02'),
+INSERT INTO employee_payroll(name, salary, start) VALUES
+('Bill', 1000000.00, '2020-02-02'),
 ('Terisa', 20000000.00, '2018-02-02'),
 ('Charlie', 30000000.00, '2018-02-02');
 
 #UC4
 #Ability to retrieve all data of table
-select * from employee_payroll_service;
+SELECT * FROM employee_payroll_service;
 					
 #UC5
 #Ability to retrieve salary of particular employee
-select salary from employee_payroll_service where name = 'Bill';
+SELECT salary FROM employee_payroll_service WHERE name = 'Bill';
 #Ability to retrieve employee data for a particular range of date
-select * from employee_payroll_service
-where start between cast('2018-01-01' as date) and date(now());	
+SELECT * FROM employee_payroll_service
+WHERE start BETWEEN CAST('2018-01-01' AS DATE) and DATE(NOW());	
 
 #UC6
 #Ability to alter table data with addition of a new field gender
-alter table employee_payroll_service add gender char(1) after name;
+ALTER table employee_payroll add gender char(1) AFTER name;
 #Ability to set gender using 'where' condition with employee name
-update employee_payroll_service set gender = 'F' where name = 'Terisa';
-update employee_payroll_service set gender = 'M' where name = 'Billie' 
-or name = 'Charlie';
+update employee_payroll set gender = 'F' WHERE name = 'Terisa';
+update employee_payroll set gender = 'M' WHERE name = 'Billie' 
+OR name = 'Charlie';
 
 #UC7
 #Ability to find sum, average, min, max, count by male or female
-select sum(salary) from employee_payroll_service where gender = 'F'
-group by gender;
-select gender, sum(salary) from employee_payroll_service
-where gender = 'M' group by gender;
-select gender, avg(salary) from employee_payroll_service group by gender;
-select gender, min(salary) from employee_payroll_service group by gender;
-select gender, max(salary) from employee_payroll_service group by gender;
-select gender, count(salary) from employee_payroll_service group by gender;
+SELECT sum(salary) FROM employee_payroll WHERE gender = 'F'
+GROUP BY gender;
+SELECT gender, sum(salary) FROM employee_payroll
+WHERE gender = 'M' GROUP BY gender;
+SELECT gender, avg(salary) FROM employee_payroll GROUP BY gender;
+SELECT gender, min(salary) FROM employee_payroll GROUP BY gender;
+SELECT gender, max(salary) FROM employee_payroll GROUP BY gender;
+SELECT gender, count(salary) FROM employee_payroll GROUP BY gender;
+
+#UC8
+#Ability to extend employee_payroll data to store employee information like 
+#employee phone, address and department
+ALTER TABLE employee_payroll ADD phone_number VARCHAR(250) AFTER name;
+ALTER TABLE employee_payroll ADD address VARCHAR(250) AFTER phone_number;
+ALTER TABLE employee_payroll ADD department VARCHAR(150) NOT NULL AFTER address;
+ALTER TABLE employee_payroll ALTER address SET DEFAULT 'TBD';
